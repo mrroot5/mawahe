51fcec5a276167ec841d24e18f4d71c8b1c2c98d
Adrián Garrido <agarrido@qdqmedia.com>
2019-12-09 11:48:06 +0100

    Added tattoo page basic design

--- pages/gallery/tattoos.vue
+++ pages/gallery/tattoos.vue
@@ -1,15 +1,83 @@
 <template>
-  <img src="http://placekitten.com/200/200" alt="Example image" srcset="" />
+  <div class="container">
+    <navbar class="navbar" />
+    <div class="main-content">
+      <gallery :gallery-data="gallery" :overlay-class="overlayCss" />
+    </div>
+    <main-footer class="footer" />
+  </div>
 </template>
 
 <script>
+import Navbar from '~/components/commons/Navbar'
+import MainFooter from '~/components/commons/MainFooter'
+import Gallery from '~/components/views/Gallery'
+
 export default {
+  components: {
+    Navbar,
+    MainFooter,
+    Gallery
+  },
   head() {
     return {
-      titleTemplate: `Tattoos - %s`
+      titleTemplate: `Tattoo - %s`
+    }
+  },
+  data() {
+    return {
+      overlayCss: 'secondary-color-bg',
+      gallery: [
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        },
+        {
+          url: '/img/tattoo/tattoo-square-300.jpg',
+          alt: 'Sample image'
+        }
+      ]
     }
   }
 }
 </script>
-
-<style></style>
