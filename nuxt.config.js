0a1ea246aef659e5145823153c6888a2f65d4ffe
Adrián G <mroot5@outlook.es>
2020-02-16 14:16:05 +0100

    Official nuxt amp config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,24 @@
+const modifyHtml = (html) => {
+  // Add amp-custom tag to added CSS
+  html = html.replace(/<style data-vue-ssr/g, '<style amp-custom data-vue-ssr')
+  // Remove every script tag from generated HTML
+  html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
+  // Add AMP script before </head>
+  const ampScript =
+    '<script async src="https://cdn.ampproject.org/v0.js"></script>'
+  html = html.replace('</head>', ampScript + '</head>')
+  return html
+}
+
 export default {
   mode: 'universal',
   /*
    ** Headers of the page
    */
   head: {
+    // htmlAttrs: {
+    //   amp: 'amp'
+    // },
     title: `@${process.env.npm_package_name}`,
     meta: [
       { charset: 'utf-8' },
@@ -19,16 +34,24 @@ export default {
         content: 'Adrián Garrido, https://github.com/mrroot5'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'canonical', href: '/' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Roboto'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  // loading: { color: '#fff' },
+  loading: false, // Disable loading bar since AMP will not generate a dynamic page
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['~/assets/main.css'],
   /*
    ** Plugins to load before mounting the App
    */
@@ -63,5 +86,19 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  render: {
+    // Disable resourceHints since we don't load any scripts for AMP
+    resourceHints: false
+  },
+  hooks: {
+    // This hook is called before generatic static html files for SPA mode
+    'generate:page': (page) => {
+      page.html = modifyHtml(page.html)
+    },
+    // This hook is called before rendering the html to the browser
+    'render:route': (url, page, { req, res }) => {
+      page.html = modifyHtml(page.html)
+    }
   }
 }
